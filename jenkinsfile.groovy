fc3621665653d6e505a493dae61a137d3941801d
tsharmilareddy <tsharmilareddy47@gmail.com>
2022-11-01 19:28:15 +0530

    updated jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,9 +4,9 @@ node{
         sh "ssh root@172.31.90.67 'sudo yum install httpd -y' "
     }
 
-    stage ('start the apache'){
-    sh "ssh root@172.31.90.67 'sudo systemctl start httpd' "
-    }
+    // stage ('start the apache'){
+    // sh "ssh root@172.31.90.67 'sudo systemctl start httpd' "
+    // }
 
 
 }
